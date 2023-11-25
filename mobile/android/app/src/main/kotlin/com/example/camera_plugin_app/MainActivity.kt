621ffb1dd7d55a1baef57eea44ff6172b7ae42ef
bellujrb <98968823+bellujrb@users.noreply.github.com>
2023-11-25 14:26:14 -0300

    remove mobile

--- mobile/android/app/src/main/kotlin/com/example/camera_plugin_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.camera_plugin_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile/lib/core/extensions/build_context_utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension BuildContextUtils on BuildContext {
-  double get mediaHeight => MediaQuery.of(this).size.height;
-  double get mediaWidth => MediaQuery.of(this).size.width;
-}
